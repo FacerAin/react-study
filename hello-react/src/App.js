23a48ebb081e17a0aed49bbae93e9194295109a9
Yong woo Song <ywsong.dev@kakao.com>
2020-01-06 01:02:30 +0900

    Update LifeCycle Study

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import Say from "./Say";
@@ -6,6 +6,9 @@ import EventPractice from "./EventPractice";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
 import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+import Info from "./Info";
 /*
 class App extends Component {
   render() {
@@ -20,9 +23,46 @@ class App extends Component {
   }
 }
 */
+/*
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 class App extends Component {
+  state = {
+    color: "#000000"
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}></LifeCycleSample>
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
+*/
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
+};
 export default App;
--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-
+import React, { Component, useState } from "react";
+/*
 class Counter extends Component {
   state = {
     number: 0,
@@ -30,5 +30,17 @@ class Counter extends Component {
     );
   }
 }
-
+*/
+const Counter = () => {
+  const [value, setValue] = useState(0);
+  return (
+    <div>
+      <p>
+        현재 카운터의 값은 <b>{value}</b>입니다.
+      </p>
+      <button onClick={() => setValue(value + 1)}>+1</button>
+      <button onClick={() => setValue(value - 1)}>-1</button>
+    </div>
+  );
+};
 export default Counter;
