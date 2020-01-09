12f208ae4082caed3cb0b1e6941d602615443aa3
Yong woo Song <ywsong.dev@kakao.com>
2020-01-09 11:22:09 +0900

    Init Todo

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SassComponent from "./SassComponent";
-
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 /*
 function App() {
   return (
@@ -52,7 +53,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponent />
       </div>
     );
   }
