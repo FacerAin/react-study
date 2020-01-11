9644b7eaeb2c715fa5b49afd53fca1ae9031822b
Yong woo Song <ywsong.dev@kakao.com>
2020-01-11 22:40:54 +0900

    Update Readme

--- news-viewer/src/Components/NewsList.js
+++ news-viewer/src/Components/NewsList.js
@@ -7,7 +7,6 @@ const NewsListBlock = styled.div`
   box-sizing: border-box;
   padding-bottom: 3rem;
   width: 768px;
-
   margin: 0 auto;
   margin-top: 2rem;
   @media screen and (max-width: 768px) {
