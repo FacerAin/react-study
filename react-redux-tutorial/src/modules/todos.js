e0f43a56c03227bb30c0dacdac019da180368f29
Yong woo Song <ywsong.dev@kakao.com>
2020-01-14 00:17:56 +0900

    Update react- state management plan

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,8 +1,57 @@
+//Tutorial for React redux state Management using Todo.
+
+import { createAction, handleActions } from 'redux-actions';
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
 const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: '리덕스 기초 배우기',
+      done: true,
+    },
+    {
+      id: 2,
+      text: '리액트와 리덕스 사용하기',
+      done: true,
+    },
+  ],
+};
+export const changeInput = createAction(CHANGE_INPUT, input => input);
+let id = 3;
+export const insert = createAction(INSERT, text => ({
+  id: id++,
+  text,
+  done: false,
+}));
 
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
+
+const todos = handleActions(
+  {
+    [changeInput]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, action) => ({
+      ...state,
+      todos: state.todos.concat(action.payload),
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map(todo =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo,
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter(todo => todo.id !== id),
+    }),
+  },
+  initialState,
+);
+/*
 export const changeInput = input => ({
   type: CHANGE_INPUT,
   input,
@@ -69,5 +118,5 @@ function todos(state = initialState, action) {
       return state;
   }
 }
-
+*/
 export default todos;
