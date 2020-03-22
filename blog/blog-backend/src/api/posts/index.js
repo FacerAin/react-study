b719852de58ddcefd56cdfeb36b6bfa560356135
Yong woo Song <ywsong.dev@kakao.com>
2020-03-22 11:32:58 +0900

    Update Database Rest API

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,12 +1,12 @@
-const Router = require('koa-router');
-const PostsCtrl = require('./posts.ctrl');
+import Router from 'koa-router';
+import * as postsCtrl from './posts.ctrl';
+
 const posts = new Router();
 
-posts.get('/', PostsCtrl.list);
-posts.post('/', PostsCtrl.write);
-posts.get('/:id', PostsCtrl.read);
-posts.delete('/:id', PostsCtrl.remove);
-posts.put('/:id', PostsCtrl.replace);
-posts.patch('/:id', PostsCtrl.update);
+posts.get('/', postsCtrl.list);
+posts.post('/', postsCtrl.write);
+posts.get('/:id', postsCtrl.read);
+posts.delete('/:id', postsCtrl.remove);
+posts.patch('/:id', postsCtrl.update);
 
-module.exports = posts;
+export default posts;
