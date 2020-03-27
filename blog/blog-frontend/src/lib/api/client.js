7dba9acc68c7e4b1f8fd41e9ff90c442d0a00a9c
Yong woo Song <ywsong.dev@kakao.com>
2020-03-27 21:29:04 +0900

    Update Proxy Setting

---
+++ blog/blog-frontend/src/lib/api/client.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+const client = axios.create();
+
+/*
+Global Client Setting
+
+*/
+export default client;
--- blog/blog-frontend/src/modules/auth.js
+++ blog/blog-frontend/src/modules/auth.js
@@ -1,13 +1,41 @@
 import { createAction, handleActions } from 'redux-actions';
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+import produce from 'immer';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+const CHAGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-const initalState = {};
+export const changeField = createAction(
+  CHAGE_FIELD,
+  ({ form, key, value }) => ({
+    form,
+    key,
+    value,
+  }),
+);
+export const initializeForm = createAction(INITIALIZE_FORM, form => form);
+
+const initalState = {
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  login: {
+    username: '',
+    password: '',
+  },
+};
 
 const auth = handleActions(
   {
-    [SAMPLE_ACTION]: (state, action) => state,
+    [CHAGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, draft => {
+        draft[form][key] = value;
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initalState[form],
+    }),
   },
   initalState,
 );
