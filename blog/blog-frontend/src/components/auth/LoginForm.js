3bcc5ee7d1b5bfc9b0b8f2404a76bfa5955e2445
Yong woo Song <ywsong.dev@kakao.com>
2020-03-28 23:55:22 +0900

    Update AuthForm Error

--- blog/blog-frontend/src/components/auth/LoginForm.js
+++ blog/blog-frontend/src/components/auth/LoginForm.js
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { withRouter } from 'react-router-dom';
+import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
 
-const LoginForm = () => {
+const LoginForm = ({ history }) => {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const [error, setError] = useState(null);
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   }));
 
   const onChange = e => {
@@ -22,20 +28,41 @@ const LoginForm = () => {
 
   const onSubmit = e => {
     e.preventDefault();
+    const { username, password } = form;
+    dispatch(login({ username, password }));
   };
 
   useEffect(() => {
     dispatch(initializeForm('login'));
   }, [dispatch]);
 
+  useEffect(() => {
+    if (authError) {
+      console.log('Error Occured');
+      console.log(authError);
+      setError('로그인 실패');
+      return;
+    }
+    if (auth) {
+      console.log('Login Success');
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      history.push('/');
+    }
+  }, [history, user]);
   return (
     <AuthForm
       type="login"
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     ></AuthForm>
   );
 };
 
-export default LoginForm;
+export default withRouter(LoginForm);
